9a3649bca1b065aa9c28ee7876685692b499cb7d
Lee Jong Min <9105lgm@naver.com>
2023-04-12 00:23:21 +0900

    small : 파일 정리

--- src/pages/department/management/account/index.tsx
+++ src/pages/department/management/account/index.tsx
@@ -7,6 +7,7 @@ import { Button, Pagination, Space, Table, Tag } from "antd"
 import { NextPage } from "next"
 import ManagementSearch from "./ManagementSearch"
 import { ColumnsType } from "antd/es/table"
+import { DUMP_DATA } from "./dumpData"
 
 interface DataType {
   key: string;
@@ -15,44 +16,6 @@ interface DataType {
   address: string;
   tags: string[];
 }
-
-const data: DataType[] = [
-  {
-    name:"이종민",
-    age:18,
-    gender:"남",
-    status:"리더",
-    role:"관리자",
-    leader:"조예인",
-    phoneNumber:"010-5485-9349",
-  },
-  {
-    name:"조예인",
-    age:10,
-    gender:"여",
-    status:"코디",
-    role:"관리자",
-    leader:"조예인",
-    phoneNumber:"010-5485-9349",
-  },
-  {
-    name:"우상욱",
-    age:8,
-    gender:"남",
-    status:"조원",
-    leader:"조예인",
-    phoneNumber:"010-5485-9349",
-  },
-  {
-    name:"이종민",
-    age:18,
-    gender:"남",
-    status:"리더",
-    role:"관리자",
-    leader:"조예인",
-    phoneNumber:"010-5485-9349",
-  },
-];
 
 const ManagementAccount : NextPage = () => {
 
@@ -113,7 +76,7 @@ const ManagementAccount : NextPage = () => {
       <ManagementSearch/>
       <GRTable
         columns={columns} 
-        dataSource={data}
+        dataSource={DUMP_DATA}
       />
     </div>
   )
