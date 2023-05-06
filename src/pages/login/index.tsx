2a09abfb1d32fbc6bf1de33458c8d2eb1b6252c7
Lee Jong Min <9105lgm@naver.com>
2023-05-07 02:21:29 +0900

    small : login component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,52 @@
-export default function Login() {
-  return <div>Login</div>;
+import GRButton from "@component/base/button/GRButton";
+import GRText from "@component/base/text/GRText";
+import GRTextInput from "@component/base/text/GRTextInput";
+import GRFlexView from "@component/base/view/GRFlexView";
+import GRView from "@component/base/view/GRView";
+import styled from "@emotion/styled";
+import Image from "next/image";
+
+const Login = () => {
+  return(
+    <LoginContainer>
+      <GRView width={14} height={10}>
+        <GRView style={{ position:'relative' }}>
+          <Image src={"/logo_name.png"} fill={true} alt={"logo"} style={{ objectFit:"contain" }} />
+        </GRView>
+        <GRFlexView>
+          <GRView marginBottom={1}>
+            <GRText marginBottom={0.5}>아이디</GRText>
+            <GRTextInput /> 
+          </GRView>
+          <GRView marginBottom={1}>
+            <GRText marginBottom={0.5}>비밀번호</GRText>
+            <GRTextInput /> 
+          </GRView>
+        </GRFlexView>
+        <GRFlexView>
+          <GRButton>
+            로그인
+          </GRButton>
+          <GRFlexView flexDirection={'row'}>
+            <GRButton type={'link'}>
+              카카오 문의
+            </GRButton>
+            <GRButton type={'link'}>
+              비밀번호 변경
+            </GRButton>
+          </GRFlexView>
+        </GRFlexView>
+      </GRView>
+    </LoginContainer>
+  )
 }
+
+export default Login;
+
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`
