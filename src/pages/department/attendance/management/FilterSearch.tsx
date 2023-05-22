1f7b236df49c834cb7256326f35564493a946014
Lee Jong Min <9105lgm@naver.com>
2023-05-23 00:16:32 +0900

    small : header filter ui change

--- src/pages/department/attendance/management/FilterSearch.tsx
+++ src/pages/department/attendance/management/FilterSearch.tsx
@@ -4,6 +4,7 @@ import GRSelect from '@component/base/dataEntry/GRSelect'
 import GRText from '@component/base/text/GRText'
 import GRTextInput from '@component/base/text/GRTextInput'
 import GRFlexView from '@component/base/view/GRFlexView'
+import GRView from '@component/base/view/GRView'
 import React, { FC, useCallback } from 'react'
 
 type FilterSearch = {
@@ -17,52 +18,54 @@ const FilterSearch : FC<FilterSearch> = () => {
     },[])
 
   return (
-    <GRFlexView >
-        <GRFlexView flexDirection={"row"} alignItems={"center"} marginBottom={1}>
-            <GRText marginRight={1} weight={"bold"} fontSize={"b5"} width={5}>
-                날짜
-            </GRText>
-            <GRDatePicker picker={"month"} width={15} />
-        </GRFlexView>
-        <GRFlexView flexDirection={"row"} alignItems={"center"}>
-            <GRText marginRight={1} weight={"bold"} fontSize={"b5"} width={5}>
-                검색 조건
-            </GRText>
-            <GRSelect
-                width={15}
-                options={[
-                    {
-                        label: "이름",
-                        value: "name"
-                    },
-                    {
-                        label: "코디",
-                        value: "cordi"
-                    },
-                ]}
-            />
-            <GRText marginLeft={1}  weight={"bold"} fontSize={"b5"} width={5}>
-                코디
-            </GRText>
-            <GRSelect
-                width={15}
-                options={[
-                    {
-                        label: "이종민",
-                        value: "10"
-                    },
-                    {
-                        label: "우상욱",
-                        value: "11"
-                    },
-                ]}
-            />
-            <GRFlexView alignItems={"flex-end"}>
-                <GRButton onClick={onClickSearch}>
-                    조회
-                </GRButton>
+    <GRFlexView flexDirection={"row"}>
+        <GRFlexView>
+            <GRFlexView flexDirection={"row"} alignItems={"center"} marginBottom={1}>
+                <GRText weight={"bold"} fontSize={"b5"} width={5}>
+                    날짜
+                </GRText>
+                <GRDatePicker picker={"month"} width={15} />
+                <GRText marginLeft={1} weight={"bold"} fontSize={"b5"} width={5}>
+                    검색 조건
+                </GRText>
+                <GRSelect
+                    style={{ flex:1 }}
+                    options={[
+                        {
+                            label: "이름",
+                            value: "name"
+                        },
+                        {
+                            label: "코디",
+                            value: "cordi"
+                        },
+                    ]}
+                />
+            </GRFlexView>
+            <GRFlexView flexDirection={"row"} alignItems={"center"}>
+                <GRText weight={"bold"} fontSize={"b5"} width={5}>
+                    코디
+                </GRText>
+                <GRSelect
+                    style={{ flex:1 }}
+                    options={[
+                        {
+                            label: "이종민",
+                            value: "10"
+                        },
+                        {
+                            label: "우상욱",
+                            value: "11"
+                        },
+                    ]}
+                />
             </GRFlexView>
         </GRFlexView>
+        <GRView>
+            <GRButton onClick={onClickSearch} marginLeft={2}>
+                조회
+            </GRButton>
+        </GRView>
       </GRFlexView>
   )
 }
