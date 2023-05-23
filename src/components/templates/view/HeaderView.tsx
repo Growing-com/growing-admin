0736f7946d648b401b7c444fcc7aedd67914bbba
Lee Jong Min <9105lgm@naver.com>
2023-05-24 01:00:44 +0900

    small : making info on header view title

--- src/components/templates/view/HeaderView.tsx
+++ src/components/templates/view/HeaderView.tsx
@@ -1,25 +1,42 @@
 import GRText from '@component/base/text/GRText'
 import GRFlexView from '@component/base/view/GRFlexView'
-import { Divider } from 'antd'
+import GRView from '@component/base/view/GRView'
+import { Alert, Divider } from 'antd'
 import React, { type FC, ReactNode } from 'react'
+import { GRStylesConfig } from 'styles'
 
 type tHeaderView = {
   title: string;
   headerComponent?: ReactNode;
   subComponent?: ReactNode;
+  titleInfoType?:'success' | 'info' | 'warning' | 'error';
+  titleInfo?: ReactNode;
 }
 
 const HeaderView: FC<tHeaderView> = ({
   title,
   headerComponent,
-  subComponent
+  subComponent,
+  titleInfoType = "warning",
+  titleInfo
 }) => {
   return (
     <GRFlexView  borderRadius={0.5} padding={"1.3rem 2rem"} marginBottom={0.5} backgroundColor="white">
         <GRFlexView flexDirection={'row'} justifyContent={"space-between"}>
-            <GRText fontSize={"h9"} weight={"bold"}>
+          <GRFlexView flexDirection={"row"}>
+            <GRText fontSize={"h9"} weight={"bold"} marginRight={GRStylesConfig.BASE_MARGIN} >
               {title}
             </GRText>
+            {titleInfo && 
+              <Alert 
+                showIcon 
+                message={titleInfo}
+                type={titleInfoType} 
+                style={{ backgroundColor: "white" }}
+                banner={true}
+              />
+            }
+          </GRFlexView>
             {headerComponent}
         </GRFlexView>
         {subComponent &&
