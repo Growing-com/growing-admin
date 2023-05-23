ab00817a6a7a8dc5d6dfb536b2c41fd699b0aea4
Lee Jong Min <9105lgm@naver.com>
2023-05-23 21:32:12 +0900

    feat : attendance management page

--- src/pages/department/attendance/management/index.tsx
+++ src/pages/department/attendance/management/index.tsx
@@ -1,12 +1,14 @@
 import HeaderView from '@component/templates/view/HeaderView';
 import { NextPage } from 'next';
-import React from 'react'
+import React, { useMemo } from 'react'
 import FilterSearch from './FilterSearch';
 import GRTable from '@component/base/GRTable';
 import GRFlexView from '@component/base/view/GRFlexView';
 import GRButton from '@component/base/button/GRButton';
 import ExcelButton from '@component/templates/button/ExcelButton';
 import { ColumnType } from 'antd/es/table';
+import { getSundayOfMonth } from 'utils/DateUtils';
+import dayjs from 'dayjs';
 
 const DATA = [{
     cordi:"123",
@@ -25,13 +27,27 @@ type tAttendanceTable = {
 }
 
 const AttendanceManagementPage: NextPage = () => {
-
-    const columns: ColumnType<tAttendanceTable>[] = [
+    const renderAddDay = () => {
+      const sundays = getSundayOfMonth();
+      return sundays.map( (day: dayjs.Dayjs) => {
+        const dayKey = day.format("MM/DD")
+        return {
+          title: dayKey,
+          dataIndex: dayKey,
+          key: dayKey,
+          align:'center',
+        }
+      });
+    }
+    const columns: ColumnType<tAttendanceTable>[] = useMemo(() =>
+    [
         {
           title: '코디',
           dataIndex: 'cordi',
           key: 'cordi',
           align:'center',
+          width: '5rem',
+          fixed: 'left',
           render: (text) => <a>{text}</a>,
         },
         {
@@ -39,18 +55,24 @@ const AttendanceManagementPage: NextPage = () => {
           dataIndex: 'leader',
           key: 'leader',
           align:'center',
+          fixed: 'left',
+          width: '5rem',
         },
         {
           title: '이름',
           dataIndex: 'name',
           key: 'name',
           align:'center',
+          fixed: 'left',
+          width: '5rem',
         },
         {
           title: '학년',
           dataIndex: 'grade',
           key: 'grade',
           align:'center',
+          fixed: 'left',
+          width: '5rem',
         },
         {
           title: '성별',
@@ -58,8 +80,10 @@ const AttendanceManagementPage: NextPage = () => {
           key: 'gender',
           align:'center',
           fixed: 'left',
-        }
-    ];
+          width: '5rem',
+        },
+        ...renderAddDay()
+    ],[]);
     
   return (
     <>
@@ -75,6 +99,7 @@ const AttendanceManagementPage: NextPage = () => {
               total:100,
               defaultPageSize:10
           }}
+          scroll={{ x: 1300 }}
         />
     </>
   )
