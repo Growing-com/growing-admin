b6512d44e30009c00a1b76fae1f22ced6e5b8e79
Lee Jong Min <9105lgm@naver.com>
2023-05-25 00:41:50 +0900

    small : styles file name to GRStylesConfig

---
+++ src/styles/GRStylesConfig.ts
@@ -0,0 +1,12 @@
+type tGRStylesConfig = {
+    BASE_MARGIN: number;
+    BOX_SHOWDOW: any;
+    BOX_GREEN_SHOWDOW: any;
+}
+const GRStylesConfig : tGRStylesConfig = {
+    BASE_MARGIN: 0.5,
+    BOX_SHOWDOW: "2px 4px 12px rgba(0,0,0,.08)",
+    BOX_GREEN_SHOWDOW: "2px 4px 12px rgba(32, 200, 149,.3)",
+}
+
+export default GRStylesConfig;
