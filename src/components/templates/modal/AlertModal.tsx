3f9c7590753d5243dc81a83e739fad121d69cad0
Lee Jong Min <9105lgm@naver.com>
2023-05-26 01:20:39 +0900

    feat : AlertModal add

---
+++ src/components/templates/modal/AlertModal.tsx
@@ -0,0 +1,57 @@
+import GRButton from '@component/base/button/GRButton'
+import GRModal, { tGRModal } from '@component/base/modal/GRModal'
+import GRText from '@component/base/text/GRText'
+import GRFlexView from '@component/base/view/GRFlexView'
+import GRView from '@component/base/view/GRView'
+import { css } from '@emotion/react'
+import React, { FC } from 'react'
+
+type tAlertModal = {
+    onCancelClickButton?: () => void;
+    cancelButtonText?: string;
+    onOkClickButton?: () => void;
+    okButtonText?: string;
+    description: string;
+} & tGRModal
+
+const AlertModal: FC<tAlertModal> = ({
+    onCancelClickButton,
+    cancelButtonText,
+    onOkClickButton,
+    okButtonText,
+    description,
+    ...props
+}) => {
+  return (
+    <GRModal 
+        {...props} 
+        centered 
+        width={"24rem"} 
+        css={css`
+            .ant-modal-content{
+                border-radius: 1rem;
+            }
+        `}
+    >
+        <GRFlexView>
+            <GRFlexView>
+                <GRFlexView justifyContent={"center"} alignItems={"center"} style={{ minHeight:"7rem" }}>
+                    <GRText fontSize={"b2"} weight={"bold"}>
+                        {description ?? ""}
+                    </GRText>
+                </GRFlexView>
+            </GRFlexView>
+            <GRFlexView flexDirection={"row"} justifyContent={"center"}>
+                <GRButton onClick={onCancelClickButton} key={"modal-cancel-button"} marginRight={2} size={"large"}>
+                    {cancelButtonText ?? "취소"}
+                </GRButton>
+                <GRButton onClick={onOkClickButton} key={"modal-ok-button"} size={"large"}>
+                    {okButtonText ?? "확인"}
+                </GRButton>
+            </GRFlexView>
+        </GRFlexView>
+    </GRModal>
+  )
+}
+
+export default AlertModal
