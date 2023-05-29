f0cbb1fbfd7cefd0d81067661abd20b3e8de8c6e
Lee Jong Min <9105lgm@naver.com>
2023-05-29 16:58:51 +0900

    feat : prettier & lint apply

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rolus: {}
+};
---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  printWidth: 80,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+
+  singleQuote: false,
+  bracketSpacing: true,
+  semi: true,
+  useTabs: false,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+  trailingComma: "none",
+  importOrder: [
+    "^react$",
+    "^next(/(.*))?$",
+    "<THIRD_PARTY_MODULES>",
+    "^public/(.*)$",
+    "^~/(.*)$",
+    "^[./]"
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+};
