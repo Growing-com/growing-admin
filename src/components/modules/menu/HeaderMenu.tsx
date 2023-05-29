237b9170f767188d961be57b6e9a112af0373f45
Lee Jong Min <9105lgm@naver.com>
2023-05-29 11:37:54 +0900

    small : logout popover module add

--- src/components/modules/menu/HeaderMenu.tsx
+++ src/components/modules/menu/HeaderMenu.tsx
@@ -1,11 +1,13 @@
 import { UserOutlined } from '@ant-design/icons';
 import GRButtonText from '@component/base/button/GRTextButton';
+import GRText from '@component/base/text/GRText';
 import GRFlexView from '@component/base/view/GRFlexView';
 import GRView from '@component/base/view/GRView';
 import styled from '@emotion/styled';
 import { Avatar, Popover } from 'antd';
 import Image from "next/image";
 import { useRouter } from "next/router";
+import GRStylesConfig from 'styles/GRStylesConfig';
 import { Color } from 'styles/colors';
 
 const HeaderMenu = () => {
@@ -17,29 +19,39 @@ const HeaderMenu = () => {
             </GRView>
             <GRFlexView justifyContent={"space-between"} flexDirection={"row"} >
                 <GRFlexView justifyContent={"flex-start"} flexDirection={"row"}>
-                    <GRButtonText type={"link"} textColor={Color.green200}  onClick={() => {}}>
+                    <GRButtonText textColor={Color.green200}  onClick={() => {}}>
                         부서
                     </GRButtonText>
-                    <GRButtonText type={"link"} textColor={Color.green200}  onClick={() => router.push('/login')}>
+                    <GRButtonText textColor={Color.green200}  onClick={() => router.push('/login')}>
                         로그인
                     </GRButtonText>
                 </GRFlexView>
                 <Popover 
                     placement="bottom" 
-                    title={"하이"} 
+                    trigger={"click"}
                     content={() => (
-                        <div>
-                        !
-                        </div>
+                        <GRView width={10}>
+                                <GRText weight={"bold"} fontSize={"b4"}>
+                                    이종민
+                                </GRText>
+                            <GRFlexView flexDirection={"row"} alignItems={"flex-end"}>
+                                <GRText fontSize={"b7"}>18학년 리더</GRText>
+                                <GRText fontSize={"b7"} weight={"bold"} color={Color.grey40} marginLeft={GRStylesConfig.BASE_MARGIN}>
+                                    관리자
+                                </GRText>
+                            </GRFlexView>
+                            <GRFlexView alignItems={"flex-end"} marginTop={GRStylesConfig.BASE_MARGIN}>
+                                <GRButtonText width={"100%"} buttonType={"default"}>로그아웃</GRButtonText>
+                            </GRFlexView>
+                        </GRView>
                     )} 
-                    trigger="click"
                 >
                     <Avatar 
                         style={{ 
                             backgroundColor: Color.green200,
                             // color:Color.green200,
                             // borderColor:Color.green200,
-                            marginRight: "4rem"
+                            marginRight: "6rem"
                         }} 
                         icon={<UserOutlined rev={undefined} />} 
                     />
