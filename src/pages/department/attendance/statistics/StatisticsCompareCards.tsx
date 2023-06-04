4d4b0a22b5d45fdc470aaf9f5dfbb7acdf599a04
Lee Jong Min <9105lgm@naver.com>
2023-06-04 19:06:49 +0900

    small : statistics compare card ui change

--- src/pages/department/attendance/statistics/StatisticsCompareCards.tsx
+++ src/pages/department/attendance/statistics/StatisticsCompareCards.tsx
@@ -1,10 +1,10 @@
+import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
 import GRText from "@component/base/text/GRText";
 import GRFlexView from "@component/base/view/GRFlexView";
 import GRView from "@component/base/view/GRView";
 import { css } from "@emotion/react";
-import { Descriptions } from "antd";
-import { Color } from "styles/colors";
 import GRStylesConfig from "styles/GRStylesConfig";
+import { Color } from "styles/colors";
 const customBox = css`
   /* border: 0.1rem solid ${Color.grey100}; */
   /* box-shadow: rgba(32, 200, 113, 0.1) 4px 1px 20px -5px, rgba(0,0,0, 0.1) 0px 3px 7px -3px; */
@@ -23,36 +23,78 @@ const StatisticsCompareCards = () => {
         ⚡️ 지난주 대비
       </GRText>
       <GRFlexView flexDirection={"row"} marginBottom={1}>
-        <GRView padding={1} marginRight={1} customCss={customBox}>
-          <Descriptions title={"출석율"} layout={"vertical"} colon={false}>
-            <Descriptions.Item label="2023.05.23">200</Descriptions.Item>
-            <Descriptions.Item label="2023.05.30">180</Descriptions.Item>
-          </Descriptions>
-        </GRView>
-        <GRView padding={1} marginLeft={1} customCss={customBox}>
-          <Descriptions title={"결석"} layout={"vertical"} colon={false}>
-            <Descriptions.Item label="2023.05.23">200</Descriptions.Item>
-            <Descriptions.Item label="2023.05.30">180</Descriptions.Item>
-          </Descriptions>
-        </GRView>
-      </GRFlexView>
-      <GRFlexView flexDirection={"row"}>
-        <GRView padding={1} marginRight={1} customCss={customBox}>
-          <Descriptions title={"새등록"} layout={"vertical"} colon={false}>
-            <Descriptions.Item label="2023.05.23">200</Descriptions.Item>
-            <Descriptions.Item label="2023.05.30">180</Descriptions.Item>
-          </Descriptions>
-        </GRView>
-        <GRView padding={1} marginLeft={1} customCss={customBox}>
-          <Descriptions
-            title={"새가족 출석율"}
-            layout={"vertical"}
-            colon={false}
-          >
-            <Descriptions.Item label="2023.05.23">200</Descriptions.Item>
-            <Descriptions.Item label="2023.05.30">180</Descriptions.Item>
-          </Descriptions>
-        </GRView>
+        <GRFlexView padding={1} marginRight={1} css={customBox}>
+          <GRText fontSize={"b6"} color={Color.grey80} weight={"bold"}>
+            전체 인원
+          </GRText>
+          <GRText fontSize={"h6"}>200</GRText>
+          <GRView>
+            <GRText color={Color.red100} weight={"bold"}>
+              -10 % <ArrowUpOutlined rev={undefined} />
+            </GRText>
+            <GRText
+              fontSize={"b7"}
+              marginLeft={GRStylesConfig.BASE_MARGIN}
+              color={Color.grey80}
+            >
+              2023-05-23 대비
+            </GRText>
+          </GRView>
+        </GRFlexView>
+        <GRFlexView padding={1} marginRight={1} css={customBox}>
+          <GRText fontSize={"b6"} color={Color.grey80} weight={"bold"}>
+            결석율
+          </GRText>
+          <GRText fontSize={"h6"}>200</GRText>
+          <GRView>
+            <GRText color={Color.blue100} weight={"bold"}>
+              +20 % <ArrowDownOutlined rev={undefined} />
+            </GRText>
+            <GRText
+              fontSize={"b7"}
+              marginLeft={GRStylesConfig.BASE_MARGIN}
+              color={Color.grey80}
+            >
+              2023-05-23 대비
+            </GRText>
+          </GRView>
+        </GRFlexView>
+        <GRFlexView padding={1} marginRight={1} css={customBox}>
+          <GRText fontSize={"b6"} color={Color.grey80} weight={"bold"}>
+            새등록
+          </GRText>
+          <GRText fontSize={"h6"}>200</GRText>
+          <GRView>
+            <GRText color={Color.blue100} weight={"bold"}>
+              +20 % <ArrowDownOutlined rev={undefined}/>
+            </GRText>
+            <GRText
+              fontSize={"b7"}
+              marginLeft={GRStylesConfig.BASE_MARGIN}
+              color={Color.grey80}
+            >
+              2023-05-23 대비
+            </GRText>
+          </GRView>
+        </GRFlexView>
+        <GRFlexView padding={1} marginRight={1} css={customBox}>
+          <GRText fontSize={"b6"} color={Color.grey80} weight={"bold"}>
+            새가족 출석율
+          </GRText>
+          <GRText fontSize={"h6"}>200</GRText>
+          <GRView>
+            <GRText color={Color.blue100} weight={"bold"}>
+              +20 % <ArrowDownOutlined rev={undefined}/>
+            </GRText>
+            <GRText
+              fontSize={"b7"}
+              marginLeft={GRStylesConfig.BASE_MARGIN}
+              color={Color.grey80}
+            >
+              2023-05-23 대비
+            </GRText>
+          </GRView>
+        </GRFlexView>
       </GRFlexView>
     </GRView>
   );
