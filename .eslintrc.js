14ec490933c812f57e7e2526ccb71c805cc2caa6
Lee Jong Min <9105lgm@naver.com>
2023-06-04 19:07:17 +0900

    fix : eslint miss role

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,5 @@ module.exports = {
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rolus: {}
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"]
 };
