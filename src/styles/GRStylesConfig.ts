71f650e47c9a56e85b3f1ecd93e47b5c0dd94010
Lee Jong Min <9105lgm@naver.com>
2023-06-05 14:10:29 +0900

    small : add padding

--- src/styles/GRStylesConfig.ts
+++ src/styles/GRStylesConfig.ts
@@ -3,12 +3,14 @@ import { CSSProperties } from "react";
 type tGRStylesConfig = {
   BASE_MARGIN: number;
   BASE_PADDING: number;
+  HORIZONTAL_PADDING: number;
   BOX_SHOWDOW: CSSProperties["boxShadow"];
   BOX_GREEN_SHOWDOW: CSSProperties["boxShadow"];
 };
 const GRStylesConfig: tGRStylesConfig = {
   BASE_MARGIN: 0.5,
   BASE_PADDING: 0.3,
+  HORIZONTAL_PADDING: 2,
   BOX_SHOWDOW: "2px 4px 12px rgba(0,0,0,.08)",
   BOX_GREEN_SHOWDOW: "2px 4px 12px rgba(32, 200, 149,.3)"
 };
