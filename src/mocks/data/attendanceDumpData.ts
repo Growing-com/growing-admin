d005da0341ec247d551fb98f89641b7940e941af
Lee Jong Min <9105lgm@naver.com>
2023-06-21 00:20:15 +0900

    feat : mock api ready to

--- src/mocks/data/attendanceDumpData.ts
+++ src/mocks/data/attendanceDumpData.ts
@@ -1,6 +1,5 @@
 export const accountDumpData = [
   {
-    id: 1,
     name: "이종민",
     age: 18,
     gender: "남",
@@ -10,7 +9,6 @@ export const accountDumpData = [
     phoneNumber: "010-5485-9349"
   },
   {
-    id: 2,
     name: "아이유",
     age: 10,
     gender: "여",
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,37 @@
 import { rest } from "msw";
 import { accountDumpData } from "./data/accountDumpData";
 
-const accountList = (req, res, ctx) => {
+export const handlers = () => {
+  return [
+    rest.get("/account", getAccountList),
+    rest.post("/account", postAccount),
+    rest.get("/attendance", getAttendanceList),
+    rest.post("/attendance", postAttendance),
+    rest.get("/attendance/statistics", getAttendanceStatistics),
+    rest.get("/department", getDepartment)
+  ];
+};
+
+const getAccountList = (req, res, ctx) => {
   return res(ctx.status(200), ctx.json(accountDumpData));
 };
 
-export const handlers = [rest.get("/accounts", accountList)];
+const postAccount = (req, res, ctx) => {
+  return res(ctx.status(200), ctx.json(accountDumpData));
+};
+
+const getAttendanceList = (req, res, ctx) => {
+  return res(ctx.status(200), ctx.json(accountDumpData));
+};
+
+const postAttendance = (req, res, ctx) => {
+  return res(ctx.status(200), ctx.json(accountDumpData));
+};
+
+const getAttendanceStatistics = (req, res, ctx) => {
+  return res(ctx.status(200), ctx.json(accountDumpData));
+};
+
+const getDepartment = (req, res, ctx) => {
+  return res(ctx.status(200), ctx.json(accountDumpData));
+};
