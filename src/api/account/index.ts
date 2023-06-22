c97682382dc208a44d8da16f93f369ceeeeedaa8
Lee Jong Min <9105lgm@naver.com>
2023-06-22 23:04:19 +0900

    small : change type

--- src/api/account/index.ts
+++ src/api/account/index.ts
@@ -1,5 +1,6 @@
 import { request } from "api";
+import { tAccount } from "./types";
 
 export const getAccountList = () => {
-  return request.get(`/account`);
+  return request.get<tAccount>(`/account`);
 };
