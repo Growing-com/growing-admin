90dbf6ae813a33ae37770b96e75649a2b94068e6
Lee Jong Min <9105lgm@naver.com>
2023-06-27 23:24:46 +0900

    small : add const variable

---
+++ src/config/const.ts
@@ -0,0 +1,27 @@
+export const STATUS_NAME = {
+  gansa: {
+    name: "간사",
+    color: "purple"
+  },
+  codi: {
+    name: "코디",
+    color: "red"
+  },
+  leader: {
+    name: "리더",
+    color: "blue"
+  },
+  member: {
+    name: "조원",
+    color: "green"
+  },
+  new_com: {
+    name: "새가족",
+    color: "gold"
+  }
+};
+
+export const ROLE_NAME = {
+  codi: "코디",
+  manager: "관리자"
+};
