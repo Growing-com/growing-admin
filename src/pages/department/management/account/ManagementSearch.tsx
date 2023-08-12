9c297db8f9a8e30b838135b6132d10f44454273b
Lee Jong Min <9105lgm@naver.com>
2023-08-12 19:37:56 +0900

    small : 조회 엔터 evnet 추가

--- src/pages/department/management/account/ManagementSearch.tsx
+++ src/pages/department/management/account/ManagementSearch.tsx
@@ -2,7 +2,9 @@ import GRButtonText from "@component/atom/button/GRTextButton";
 import GRText from "@component/atom/text/GRText";
 import GRTextInput from "@component/atom/text/GRTextInput";
 import GRFlexView from "@component/atom/view/GRFlexView";
+import useKeyPressEventListener from "hooks/useKeyPressEventListener";
 import { ChangeEvent, FC, useCallback, useState } from "react";
+
 type tManagementSearch = {
   onClickSearch: (searchText: string) => void;
 };
@@ -22,12 +24,17 @@ const ManagementSearch: FC<tManagementSearch> = ({ onClickSearch }) => {
     []
   );
 
+  useKeyPressEventListener("Enter", () => {
+    onClickSearchButton();
+  });
+
   return (
     <GRFlexView alignItems={"center"} flexDirection={"row"}>
       <GRText marginright={1} weight={"bold"} fontSize={"b5"}>
         검색
       </GRText>
       <GRTextInput
+        value={searchText}
         marginright={2}
         placeholder={"이름, 전화 번호로 검색 하세요."}
         onChange={onChangeSearchText}
