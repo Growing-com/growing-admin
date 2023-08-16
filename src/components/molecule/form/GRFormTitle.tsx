fb1814c4b807ef822f18d8d68f42ceafed28f6e9
Lee Jong Min <9105lgm@naver.com>
2023-08-16 23:07:48 +0900

    small : form title require 있을 경우 별 표시

--- src/components/molecule/form/GRFormTitle.tsx
+++ src/components/molecule/form/GRFormTitle.tsx
@@ -1,9 +1,21 @@
 import GRText from "@component/atom/text/GRText";
+import { FC } from "react";
+import GRStylesConfig from "styles/GRStylesConfig";
+import { Color } from "styles/colors";
 
-const GRFormTitle = ({ title }: { title?: string }) => {
+type tGRFormTitle = {
+  title?: string;
+  required?: boolean;
+};
+const GRFormTitle: FC<tGRFormTitle> = ({ title, required = false }) => {
   return (
-    <GRText margin={1} width={4} weight={"bold"}>
+    <GRText marginbottom={1} marginhorizontal={1} width={4} weight={"bold"}>
       {title ?? ""}
+      {!!required && (
+        <GRText marginleft={GRStylesConfig.BASE_MARGIN} color={Color.red100}>
+          *
+        </GRText>
+      )}
     </GRText>
   );
 };
