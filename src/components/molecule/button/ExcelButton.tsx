40ab624f4187cef0fd52d460a927211d057e53c3
Lee Jong Min <9105lgm@naver.com>
2023-08-24 00:05:36 +0900

    small : 엑셀 버튼 수정

--- src/components/molecule/button/ExcelButton.tsx
+++ src/components/molecule/button/ExcelButton.tsx
@@ -1,24 +1,16 @@
 import { FileExcelOutlined } from "@ant-design/icons";
 import GRButtonText from "@component/atom/button/GRTextButton";
-import GRFlexView from "@component/atom/view/GRFlexView";
-import GRStylesConfig from "styles/GRStylesConfig";
 
 const ExcelButton = ({ onClickExcel }: { onClickExcel: () => void }) => {
   return (
     // 그냥 아이콘만 보여줄지 아니면 이름도 같이 보여줄지 설정으로
-    <GRFlexView marginbottom={GRStylesConfig.BASE_MARGIN} alignItems={"end"}>
-      <GRButtonText
-        buttonType={"default"}
-        size={"large"}
-        onClick={onClickExcel}
-      >
-        <FileExcelOutlined
-          rev={undefined}
-          style={{ fontSize: "1rem", marginRight: "0.3rem" }}
-        />
-        엑셀 다운
-      </GRButtonText>
-    </GRFlexView>
+    <GRButtonText buttonType={"default"} size={"large"} onClick={onClickExcel}>
+      <FileExcelOutlined
+        rev={undefined}
+        style={{ fontSize: "1rem", marginRight: "0.3rem" }}
+      />
+      엑셀 다운
+    </GRButtonText>
   );
 };
 
