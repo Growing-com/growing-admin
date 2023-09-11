1be3128056e7ab1e9843b04c2d60744b5b87c982
Lee Jong Min <9105lgm@naver.com>
2023-09-12 07:41:56 +0900

    feat : tab items 받는 형식 통일

--- src/components/atom/GRTab.tsx
+++ src/components/atom/GRTab.tsx
@@ -1,13 +1,34 @@
-import { Tabs, TabsProps } from "antd";
-import { FC } from "react";
+import { TabPaneProps, Tabs, TabsProps } from "antd";
+import { FC, useMemo } from "react";
+import { tOptions } from "./dataEntry/dataEntryType";
 
-const GRTab: FC<TabsProps> = ({ ...props }) => {
+type tGRTab = {
+  items: tOptions;
+} & Omit<TabsProps, "items">;
+
+type tTab = {
+  key: string;
+  label: React.ReactNode;
+} & Omit<TabPaneProps, "tab">;
+
+const GRTab: FC<tGRTab> = ({ items, ...props }) => {
+  const tabItem = useMemo(() => {
+    if (items) {
+      return items.map(_item => ({
+        key: _item.value,
+        label: _item.label
+      }));
+    }
+    return [];
+  }, [items]);
+
   return (
     <Tabs
       size={"large"}
       tabBarStyle={{
         fontWeight: "bold"
       }}
+      items={tabItem as tTab[]}
       {...props}
     />
   );
