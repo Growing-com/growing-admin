78745744ead05efafbc80299c716dd9ab7da1cf1
Lee Jong Min <9105lgm@naver.com>
2023-10-07 12:36:22 +0900

    ignoreBuildErrors: true,

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true
   },
+  typescript: {
+    ignoreBuildErrors: true
+  },
   reactStrictMode: false,
   swcMinify: true,
   async redirects() {
