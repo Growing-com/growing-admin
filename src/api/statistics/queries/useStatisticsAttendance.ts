29734d86546ffb1802b4ccc54beb6ec61a494f21
Lee Jong Min <9105lgm@naver.com>
2023-10-07 10:59:07 +0900

    small : query key 이슈

--- src/api/statistics/queries/useStatisticsAttendance.ts
+++ src/api/statistics/queries/useStatisticsAttendance.ts
@@ -1,13 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getStatisticsAttendance, tGetStatisticsAttendanceParams } from "..";
-import termQueryKeys from "../../term/termQuerykeys";
+import statisticsQuerykeys from "../statisticsQuerykeys";
 
 export const useStatisticsAttendance = (
   params: tGetStatisticsAttendanceParams,
   option: "isAbsent" | "newFamily"
 ) => {
   return useQuery(
-    [termQueryKeys.TERM_STATISTICS, option],
+    [statisticsQuerykeys.STATISTICS_ATTENDANCE, option],
     async () => await getStatisticsAttendance(params),
     {
       select: _data => _data.content
--- src/api/statistics/statisticsQuerykeys.ts
+++ src/api/statistics/statisticsQuerykeys.ts
@@ -1,5 +1,6 @@
 const statisticsQuerykeys = {
-  STATISTICS_BASE: "statistics_base"
+  STATISTICS_BASE: "statistics_base",
+  STATISTICS_ATTENDANCE: "statistics_attendance"
 };
 
 export default statisticsQuerykeys;
