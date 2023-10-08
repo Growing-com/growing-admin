10c4472005d4ffd8f97c71f1ecaba2d544d6ae7a
Lee Jong Min <9105lgm@naver.com>
2023-10-08 18:52:59 +0900

    small : 통계 관련 api  추가

--- src/api/statistics/index.ts
+++ src/api/statistics/index.ts
@@ -1,4 +1,5 @@
 import { REQUEST_METHOD, request } from "api";
+import { tStatisticsAttendanceExcelOption } from "pages/department/attendance/statistics";
 
 const STATISTICS_PREFIX = "/statistics";
 
@@ -31,5 +32,39 @@ export const getStatisticsAttendance = (
     method: REQUEST_METHOD.GET,
     url: `${STATISTICS_PREFIX}/attendance`,
     params
+  });
+};
+
+type tPostStatisticsAttendanceJob = {
+  name: "weeklyAttendanceJob" | "termAttendanceJob";
+  jobParameters: {
+    /** "2023-07-30" */
+    requestDate?: string;
+    termId?: number;
+  };
+};
+
+export const postStatisticsAttendanceJob = (
+  data: tPostStatisticsAttendanceJob
+) => {
+  return request({
+    method: REQUEST_METHOD.POST,
+    url: `/job`,
+    data
+  });
+};
+
+export type tGetStatisticsAttendanceExcelParams = {
+  termId: number;
+  options: tStatisticsAttendanceExcelOption;
+};
+
+export const getStatisticsAttendanceExcel = ({
+  termId,
+  options
+}: tGetStatisticsAttendanceExcelParams) => {
+  return request({
+    method: REQUEST_METHOD.GET,
+    url: `${STATISTICS_PREFIX}/term/${termId}/${options}`
   });
 };
