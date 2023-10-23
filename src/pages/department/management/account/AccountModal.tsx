b887628040c88328d392fa050bb33aac6bd11235
Lee Jong Min <9105lgm@naver.com>
2023-10-23 23:06:52 +0900

    fix : QRO-27 수정  * [버그] 시급 - [프론트] 계정 생성시 텀 필드 추가 필요

--- src/pages/department/management/account/AccountModal.tsx
+++ src/pages/department/management/account/AccountModal.tsx
@@ -55,7 +55,8 @@ const AccountModal: FC<tAccountModal> = ({
         ..._item,
         birth: dayjs(_item.birth).format(DEFAULT_DATE_FOMAT),
         visitDate: dayjs(_item.visitDate).format(DEFAULT_DATE_FOMAT),
-        termId: 1
+        termId: 1,
+        visitTermId: 1
       };
       try {
         if (isCreate) {
