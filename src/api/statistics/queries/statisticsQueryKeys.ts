c6abbedb899d1d7efce68cdd5b21688d6c1dc8dd
Lee Jong Min <9105lgm@naver.com>
2023-11-08 01:05:58 +0900

    fix : progress check api query key 추가

--- src/api/statistics/queries/statisticsQueryKeys.ts
+++ src/api/statistics/queries/statisticsQueryKeys.ts
@@ -2,7 +2,8 @@ const statisticsQueryKeys = {
   STATISTICS_BASE: "statistics_base",
   STATISTICS_ATTENDANCE: "statistics_attendance",
   STATISTICS_ATTENDANCE_BENCH: "statistics_attendance_bench",
-  STATISTICS_ATTENDANCE_EXCEL: "statistics_attendance_excel"
+  STATISTICS_ATTENDANCE_EXCEL: "statistics_attendance_excel",
+  STATISTICS_ATTENDANCE_PROGRESS: "statistics_attendance_progress"
 };
 
 export default statisticsQueryKeys;
