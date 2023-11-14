81a477493c6cba9aeeb4f8379c2bc8600c4fd9d4
Lee Jong Min <9105lgm@naver.com>
2023-11-14 21:51:22 +0900

    fix : 출석 관리 -> 검색시 출책결과 이상하게 보이는 현상

--- src/components/templates/table/ColumDateTitleAttendanceRender.tsx
+++ src/components/templates/table/ColumDateTitleAttendanceRender.tsx
@@ -1,5 +1,9 @@
 import ColumAttendanceRender from "@component/molecule/table/ColumAttendanceRender";
-import { tAttendanceItem } from "api/attendance/types";
+import {
+  tAttendanceCheckListItem,
+  tAttendanceItem
+} from "api/attendance/types";
+import dayjs from "dayjs";
 import { head } from "lodash";
 type tColumDateTitleAttendanceRender<T> = {
   attendanceList?: T[];
@@ -9,19 +13,35 @@ const ColumDateTitleAttendanceRender = <DataType extends object>({
 }: tColumDateTitleAttendanceRender<DataType>) => {
   if (!attendanceList?.length) return {};
 
+  const filterWeeks = (attendData: tAttendanceCheckListItem[]) => {
+    const weeks = [] as string[];
+    attendData.forEach(attend => {
+      const { attendanceItems } = attend;
+      attendanceItems.forEach(item => {
+        if (item.week && !weeks.includes(item.week)) {
+          weeks.push(item.week);
+        }
+      });
+    });
+    weeks.sort((a, b) => (dayjs(a).isAfter(dayjs(b)) ? 1 : -1));
+    return weeks;
+  };
+
   const renderAddDay = (list: any[]) => {
     if (!list?.length) return [];
 
     const _attendanceItems = head(list);
+    const _weeks = filterWeeks(list);
+
     if (_attendanceItems?.attendanceItems?.length) {
-      return _attendanceItems.attendanceItems.map((item: any) => {
+      return _weeks.map(week => {
         return {
-          title: item.week,
+          title: week,
           dataIndex: "attendanceItems",
-          key: `attendanceItems-${item.week}`,
+          key: `attendanceItems-${week}`,
           align: "center",
           render: (record: tAttendanceItem[]) => {
-            const findData = record.find(r => r.week === item.week);
+            const findData = record.find(r => r.week === week);
             return (
               <ColumAttendanceRender
                 attendanceStatus={findData?.status}
