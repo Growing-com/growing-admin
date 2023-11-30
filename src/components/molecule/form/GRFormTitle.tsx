86ebcc256e519e0dacc7a2a1d86838eacbef23af
Lee Jong Min <9105lgm@naver.com>
2023-11-30 22:21:00 +0900

    feat : form title 옆에 설명 할수 있는 alert icon 추가

--- src/components/molecule/form/GRFormTitle.tsx
+++ src/components/molecule/form/GRFormTitle.tsx
@@ -1,22 +1,50 @@
 import GRText from "@component/atom/text/GRText";
+import GRFlexView from "@component/atom/view/GRFlexView";
+import GRView from "@component/atom/view/GRView";
+import { Alert, Tooltip } from "antd";
 import { FC } from "react";
 import GRStylesConfig from "styles/GRStylesConfig";
 import { Color } from "styles/colors";
 
 type tGRFormTitle = {
   title?: string;
+  alertMessage?: string;
   required?: boolean;
 };
-const GRFormTitle: FC<tGRFormTitle> = ({ title, required = false }) => {
+const GRFormTitle: FC<tGRFormTitle> = ({
+  title,
+  required = false,
+  alertMessage
+}) => {
   return (
-    <GRText marginbottom={1} marginhorizontal={1} width={4} weight={"bold"}>
-      {title ?? ""}
-      {!!required && (
-        <GRText marginleft={GRStylesConfig.BASE_MARGIN} color={Color.red100}>
-          *
+    <GRView width={5} marginhorizontal={1}>
+      <GRFlexView flexDirection={"row"} alignItems={"center"}>
+        <GRText weight={"bold"}>
+          {title ?? ""}
+          {!!required && (
+            <GRText
+              marginleft={GRStylesConfig.BASE_MARGIN}
+              color={Color.red100}
+            >
+              *
+            </GRText>
+          )}
         </GRText>
-      )}
-    </GRText>
+        {!!alertMessage && (
+          <Tooltip
+            overlayStyle={{ whiteSpace: "pre-line" }}
+            title={alertMessage}
+          >
+            <Alert
+              showIcon
+              type={"info"}
+              banner={true}
+              style={{ backgroundColor: "transparent" }}
+            />
+          </Tooltip>
+        )}
+      </GRFlexView>
+    </GRView>
   );
 };
 
