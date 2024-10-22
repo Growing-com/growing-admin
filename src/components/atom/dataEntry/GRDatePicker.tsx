725303bca85e32110d85956a7bf12b01f99ca85a
terrydkim <kdjtiger@gmail.com>
2024-10-22 13:19:32 +0900

    fix: GRFormItem ref 에러 처리

--- src/components/atom/dataEntry/GRDatePicker.tsx
+++ src/components/atom/dataEntry/GRDatePicker.tsx
@@ -4,7 +4,14 @@ import { RangePickerProps } from "antd/es/date-picker";
 import locale from "antd/lib/date-picker/locale/ko_KR";
 import dayjs, { Dayjs } from "dayjs";
 import "dayjs/locale/ko"; // 한국어 locale 추가
-import { CSSProperties, FC, useCallback, useMemo, useState } from "react";
+import { PickerRef } from "rc-picker";
+import {
+  CSSProperties,
+  forwardRef,
+  useCallback,
+  useMemo,
+  useState
+} from "react";
 import GRStylesConfig from "styles/GRStylesConfig";
 import { DEFAULT_DATE_FORMAT } from "utils/DateUtils";
 import GRTextButton from "../button/GRTextButton";
@@ -25,84 +32,77 @@ type tGRDatePicker<T extends tPickerType> = {
   ? { pickerType: T } & Omit<DatePickerProps, "onChange">
   : { pickerType: T } & RangePickerProps);
 
-const GRDatePicker: FC<tGRDatePicker<tPickerType>> = ({
-  height,
-  width,
-  format,
-  onChange,
-  picker,
-  pickerType,
-  ...props
-}) => {
-  const BaseDatePicker = useMemo(
-    () =>
-      (pickerType === "basic"
-        ? DatePicker
-        : RangePicker) as React.FC<
-        Omit<tGRDatePicker<tPickerType>, "pickerType">
-      >,
-    [pickerType]
-  );
-
-  const [date, setDate] = useState<Dayjs | null>();
-
-  const _format = useMemo(() => {
-    if (format) return format;
-    if (picker !== "week") return;
-
-    const weekOfMonth =
-      Number(dayjs(date).format("w")) -
-      Number(dayjs(date).startOf("M").format("w")) +
-      1;
-    return picker === "week"
-      ? `${dayjs(date)
-          .startOf("week")
-          .format("YYYY-MM-DD")} (${weekOfMonth}주차)`
-      : DEFAULT_DATE_FORMAT;
-  }, [date, format, picker]);
-
-  const onChangeDate = useCallback(
-    (_date: Dayjs | null) => {
-      setDate(_date);
-      onChange?.(_date);
-    },
-    [onChange]
-  );
-
-  const renderExtraFooter = () => {
-    if (picker !== "week") return;
-    return (
-      <GRFlexView
-        alignItems={"center"}
-        marginvertical={GRStylesConfig.BASE_MARGIN}
-      >
-        <GRTextButton
-          buttonType={"default"}
-          onClick={() => onChangeDate(dayjs())}
-        >
-          TODAY
-        </GRTextButton>
-      </GRFlexView>
+const GRDatePicker = forwardRef<PickerRef, tGRDatePicker<tPickerType>>(
+  ({ height, width, format, onChange, picker, pickerType, ...props }, ref) => {
+    const BaseDatePicker = useMemo(
+      () =>
+        (pickerType === "basic" ? DatePicker : RangePicker) as React.FC<
+          Omit<tGRDatePicker<tPickerType>, "pickerType"> & { ref?: PickerRef }
+        >,
+      [pickerType]
     );
-  };
 
-  return (
-    <BaseDatePicker
-      locale={locale}
-      css={css`
-        width: ${width}rem;
-        height: ${height}rem;
-      `}
-      format={_format}
-      picker={picker}
-      onChange={onChangeDate}
-      renderExtraFooter={renderExtraFooter}
-      disabledDate={(current: Dayjs) => {
-        return current && current.valueOf() > Date.now();
-      }}
-      {...props}
-    />
-  );
-};
+    const [date, setDate] = useState<Dayjs | null>();
+
+    const _format = useMemo(() => {
+      if (format) return format;
+      if (picker !== "week") return;
+
+      const weekOfMonth =
+        Number(dayjs(date).format("w")) -
+        Number(dayjs(date).startOf("M").format("w")) +
+        1;
+      return picker === "week"
+        ? `${dayjs(date)
+            .startOf("week")
+            .format("YYYY-MM-DD")} (${weekOfMonth}주차)`
+        : DEFAULT_DATE_FORMAT;
+    }, [date, format, picker]);
+
+    const onChangeDate = useCallback(
+      (_date: Dayjs | null) => {
+        setDate(_date);
+        onChange?.(_date);
+      },
+      [onChange]
+    );
+
+    const renderExtraFooter = () => {
+      if (picker !== "week") return;
+      return (
+        <GRFlexView
+          alignItems={"center"}
+          marginvertical={GRStylesConfig.BASE_MARGIN}
+        >
+          <GRTextButton
+            buttonType={"default"}
+            onClick={() => onChangeDate(dayjs())}
+          >
+            TODAY
+          </GRTextButton>
+        </GRFlexView>
+      );
+    };
+
+    return (
+      <BaseDatePicker
+        ref={ref as any}
+        locale={locale}
+        css={css`
+          width: ${width}rem;
+          height: ${height}rem;
+        `}
+        format={_format}
+        picker={picker}
+        onChange={onChangeDate}
+        renderExtraFooter={renderExtraFooter}
+        disabledDate={(current: Dayjs) => {
+          return current && current.valueOf() > Date.now();
+        }}
+        {...props}
+      />
+    );
+  }
+);
 
 export default GRDatePicker;
--- src/components/atom/dataEntry/GRSelect.tsx
+++ src/components/atom/dataEntry/GRSelect.tsx
@@ -2,7 +2,8 @@ import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import type { SelectProps } from "antd";
 import { Select } from "antd";
-import { CSSProperties } from "react";
+import type { BaseSelectRef } from "rc-select";
+import { CSSProperties, forwardRef } from "react";
 import { AreaType } from "styles/css";
 import getMargin from "styles/css/getMargin";
 import { tOptions } from "./type";
@@ -16,35 +17,38 @@ export type tGRSelect = {
 
 const { Option } = Select;
 
-const GRSelect = ({
-  options,
-  mode,
-  height,
-  width,
-  allowClear = true,
-  ...props
-}: tGRSelect) => {
-  const _margin = getMargin(props);
-  return (
-    <SelectCompo
-      mode={mode}
-      css={css`
-        width: ${width}rem;
-        height: ${height}rem;
-        ${_margin};
-      `}
-      options={options}
-      listItemHeight={10}
-      listHeight={100}
-      allowClear={allowClear}
-      {...props}
-    />
-  );
-};
+const GRSelect = forwardRef<BaseSelectRef, tGRSelect>(
+  (
+    { options, mode, height, width, allowClear = true, ...props }: tGRSelect,
+    ref
+  ) => {
+    const _margin = getMargin(props);
+    return (
+      <SelectCompo
+        ref={ref}
+        mode={mode}
+        css={css`
+          width: ${width}rem;
+          height: ${height}rem;
+          ${_margin};
+        `}
+        options={options}
+        listItemHeight={10}
+        listHeight={100}
+        allowClear={allowClear}
+        {...props}
+      />
+    );
+  }
+);
 
 export default GRSelect;
 
-const SelectCompo = styled(({ ...props }: tGRSelect) => <Select {...props} />)`
+const SelectCompo = styled(
+  forwardRef<BaseSelectRef, tGRSelect>(({ ...props }: tGRSelect, ref) => (
+    <Select {...props} ref={ref} />
+  ))
+)`
   .ant-select-selector {
     align-items: center !important;
   }
